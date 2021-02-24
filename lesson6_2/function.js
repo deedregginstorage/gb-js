22781e8f5bd3e9334aabb3cec4c1427c2fbdbb12
Anatoly <anatoliy.kozhevnikov@gmail.com>
2021-02-24 11:37:05 +0300

    added array for selected items

--- lesson6_2/function.js
+++ lesson6_2/function.js
@@ -1,9 +1,12 @@
+//корзина в процессе, за вёрстку не стреляйте(( 
+
 let items = [{name: 'Marlboro', price: 130, quantity: 0, src:'./img/marl.jpg'}, {name: 'Беломорканал', price: 30, quantity: 0, src:'./img/belomor.webp'}, {name: 'Winston', price: 110, quantity: 0, src:'./img/winston.png'}]
 
 let cardsImg = document.querySelectorAll('img');
 let card = document.querySelectorAll('.item');
 let h1 = document.querySelectorAll('h1');
 let button = document.querySelectorAll('button');
+let cartField = document.querySelector('div')
 
 for (let i = 0; i < items.length; i++) {
     cardsImg[i].src = items[i].src;
@@ -12,18 +15,17 @@ for (let i = 0; i < items.length; i++) {
     button[i].id = i;
 }
 
-function f(){
-    if (items.quantity > 0) {
-        return true
-    } else {
-        return false
-    }
-}
+let cart = [];
 
 function toCart(event) {
         let cartQuantity = event.target.id;
         ++items[cartQuantity].quantity;
-        console.log(items[cartQuantity].quantity)
-        let cart = items.filter(f)
-        console.log(cart)
+        cart = items.filter(function (e) {
+            return e.quantity > 0
+        });
+        for (let i = 0; i < cart.length; i++) {
+            let sum = cart[i].quantity * cart[i].price;
+            cartField.innerHTML = '<p>название: ' + cart[i].name + '</p>\n' + '<p> количество: \n' + cart[i].quantity + '</p\n>' + '<p>сумма: '+ sum + 'рублей </p>';
+        }
 }
+
